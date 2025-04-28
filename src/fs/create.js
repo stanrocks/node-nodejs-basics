09559959e62ea02872e66e9d8b540c5f2c2f7020
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2025-04-28 08:39:18 +0500

    feat: add create file function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import fs from 'node:fs/promises';
+
 const create = async () => {
-  // Write your code here
+  const filePath = './src/fs/files/fresh.txt';
+  const content = 'I am fresh and young';
+  const options = { flag: 'wx' };
+
+  try {
+    await fs.writeFile(filePath, content, options);
+  } catch (error) {
+    if (error.code === 'EEXIST') {
+      throw new Error('FS operation failed');
+    }
+
+    throw error;
+  }
 };
 
 await create();
