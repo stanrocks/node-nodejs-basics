f249a343ffa5c763fc0f60b33f7ab7938d5ceefd
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2025-04-29 02:13:36 +0500

    feat: add compress and decompress functions

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,23 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import { pipeline } from 'node:stream';
+import { createGzip } from 'node:zlib';
+
+const dirName = import.meta.dirname;
+const srcFile = path.join(dirName, 'files', 'fileToCompress.txt');
+const destFile = path.join(dirName, 'files', 'archive.gz');
+const gzip = createGzip();
+
 const compress = async () => {
-    // Write your code here 
+  const readable = fs.createReadStream(srcFile);
+  const writable = fs.createWriteStream(destFile);
+
+  pipeline(readable, gzip, writable, (err) => {
+    if (err) {
+      console.error('An error occurred:', err);
+      process.exitCode = 1;
+    }
+  });
 };
 
 await compress();
