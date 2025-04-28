760e52a2698122f07dfa81c2fd79d70a71cbb18f
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2025-04-28 22:43:39 +0500

    feat: add rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,25 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+
+const dirName = import.meta.dirname;
+const oldPath = path.join(dirName, 'files', 'wrongFilename.txt');
+const newPath = path.join(dirName, 'files', 'properFilename.md');
+
 const rename = async () => {
-    // Write your code here 
+  try {
+    await fs.access(newPath);
+    throw new Error('FS operation failed');
+  } catch (error) {
+    if (error.code !== 'ENOENT') {
+      throw new Error('FS operation failed');
+    }
+  }
+
+  try {
+    await fs.rename(oldPath, newPath);
+  } catch (error) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await rename();
