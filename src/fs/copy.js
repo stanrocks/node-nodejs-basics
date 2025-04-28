628885c79098896c70674c376f92dcf52676f801
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2025-04-28 22:07:31 +0500

    refactor: replace copyFile with cp method

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -5,18 +5,13 @@ const copy = async () => {
   const dirName = import.meta.dirname;
   const srcDir = path.join(dirName, 'files');
   const destDir = path.join(dirName, 'files_copy');
-  const copyMode = fs.constants.COPYFILE_EXCL;
 
   try {
-    const filesToCopy = await fs.readdir(srcDir);
+    await fs.readdir(srcDir);
 
     await fs.mkdir(destDir);
 
-    for (const file of filesToCopy) {
-      const srcFile = path.join(srcDir, file);
-      const destFile = path.join(destDir, file);
-      await fs.copyFile(srcFile, destFile, copyMode);
-    }
+    await fs.cp(srcDir, destDir, { recursive: true });
   } catch (error) {
     throw new Error('FS operation failed');
   }
