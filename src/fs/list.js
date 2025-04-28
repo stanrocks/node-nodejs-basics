44240e56df1295f0f674f23e79b3fa4f2ac5622b
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2025-04-28 23:05:21 +0500

    feat: add list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,16 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+
+const dirName = import.meta.dirname;
+const dirPath = path.join(dirName, 'files');
+
 const list = async () => {
-    // Write your code here 
+  try {
+    const files = await fs.readdir(dirPath);
+    console.log(files);
+  } catch (error) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await list();
