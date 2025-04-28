8a32df8cacb74073ccbf00a80a4d4078acf7c2ea
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2025-04-29 01:33:49 +0500

    feat: add read function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+import fs from 'node:fs';
+import path from 'node:path';
+
+const dirName = import.meta.dirname;
+const filePath = path.join(dirName, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+  const readable = fs.createReadStream(filePath);
+
+  readable.pipe(process.stdout);
+
+  readable.on('end', () => {
+    process.stdout.write('\n');
+  });
 };
 
 await read();
